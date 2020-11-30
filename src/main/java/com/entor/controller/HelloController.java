02545768d0d5cabef89a3778e279f72ee568241a
陈北辉 <2634987935@qq.com>
2020-11-30 16:27:32 +0800

    新增内容

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    private int port;
     @GetMapping("/hello")
     public String hello(){
         return "hello1111";
@@ -12,5 +15,9 @@ public class HelloController {
     @GetMapping("/index")
     public String index(){
         return "index";
+    }
+    @GetMapping("/getPost")
+    public String getPost(){
+        return "服务器端口"+port;
     }
 }
